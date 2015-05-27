8d080ef620950247fc1b84a115d4b2c865860fd0
Guillermo Platas <elbotas@gmail.com>
2015-05-27 19:35:23 +0200

    Test push build 2

--- src/main/java/NoticeBoard/BoardController.java
+++ src/main/java/NoticeBoard/BoardController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
-//TEST FOR JENKINS
+
 @Controller
 public class BoardController {
 
