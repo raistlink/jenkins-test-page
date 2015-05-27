dcec12fdb7920e5558e0820f46946d78bd07980a
Guillermo Platas <elbotas@gmail.com>
2015-05-27 21:05:11 +0200

    Deployable Spring

--- src/main/java/NoticeBoard/Application.java
+++ src/main/java/NoticeBoard/Application.java
@@ -3,6 +3,8 @@ package NoticeBoard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 @SpringBootApplication
-public class Application {
+
+public class Application extends SpringBootServletInitializer
+{
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
