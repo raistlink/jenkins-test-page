eb802cb78c4084d845f200bd4d36211da73d438e
Guillermo Platas <elbotas@gmail.com>
2015-05-27 18:06:02 +0200

    First Commit

---
+++ src/main/java/NoticeBoard/Announcement.java
@@ -0,0 +1,18 @@
+package NoticeBoard;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Work on 10/04/2015.
+ */
+public class Announcement {
+    public String name;
+    public String subject;
+    public String comment;
+
+    public Announcement(String name, String subject, String comment) {
+        this.name = name;
+        this.subject = subject;
+        this.comment = comment;
+    }
+}
---
+++ src/main/java/NoticeBoard/BoardController.java
@@ -0,0 +1,81 @@
+package NoticeBoard;
+
+import java.sql.*;
+import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class BoardController {
+
+    ArrayList<Announcement> announcements = new ArrayList<Announcement>();
+
+    @RequestMapping("")
+    public ModelAndView main(HttpSession session) throws SQLException, ClassNotFoundException{
+
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "");
+
+        String createDatabase = "CREATE DATABASE IF NOT EXISTS Announcements";
+        String createTable = "CREATE TABLE IF NOT EXISTS Anuncios" +
+                "(name VARCHAR(15)," +
+                "subject VARCHAR(15)," +
+                "comments LONGTEXT," +
+                "PRIMARY KEY (name,subject))";
+
+        Statement statement = connection.createStatement();
+        statement.execute(createDatabase);
+        connection.close();
+
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Announcements", "root", "");
+        statement = connection.createStatement();
+        statement.execute(createTable);
+
+        announcements.clear();
+
+        String query = "SELECT * FROM Anuncios";
+        Statement queryStatement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(query);
+
+        while(rs.next()){
+            String name = rs.getString("name");
+            String subject = rs.getString("subject");
+            String comments = rs.getString("comments");
+
+            Announcement newAnnouncement = new Announcement(name,subject,comments);
+            announcements.add(newAnnouncement);
+        }
+
+        return new ModelAndView("main").addObject("isNew", session.isNew()).addObject("announcements",announcements);
+    }
+    @RequestMapping("/new")
+    public ModelAndView newAnnouncement(HttpSession session){
+        String name = (String) session.getAttribute("username");
+        return new ModelAndView("new").addObject("name", name);
+    }
+    @RequestMapping("/insert")
+    public ModelAndView insert(HttpSession session, @RequestParam String name,@RequestParam String subject, @RequestParam String comment) throws SQLException, ClassNotFoundException {
+        session.setAttribute("username", name);
+        if(name != "" && subject != ""){
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Announcements", "root", "");
+            Statement statement = connection.createStatement();
+
+            String insert = "INSERT INTO Anuncios VALUES ('"+name+"','"+subject+"','"+comment+"')";
+            statement.execute(insert);
+
+        }
+        return new ModelAndView("insert");
+    }
+    @RequestMapping("/{postNumber}")
+    public ModelAndView view(@PathVariable int postNumber){
+        return new ModelAndView("view").addObject("announcement",announcements.get(postNumber));
+    }
+}
