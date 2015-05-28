bae9fbb29f205f7c5b124177859cda0acb7a4627
Guillermo Platas <elbotas@gmail.com>
2015-05-28 05:35:32 +0200

    First FunctionalTesting test

---
+++ src/test/java/NoticeBoard/FuncTest.java
@@ -0,0 +1,17 @@
+package NoticeBoard;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by Boots on 28/05/2015.
+ */
+public class FuncTest {
+
+    @Test
+    /*
+    *  @goal FunctionalTesting
+    */
+    public void funcTest() throws Exception{
+        assertEquals(1,1);
+    }
+}
