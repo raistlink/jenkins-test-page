8968a3b796c8dcdeb687655625101a286aa037e5
Guillermo Platas <elbotas@gmail.com>
2015-05-28 06:15:43 +0200

    First FunctionalTesting test working

--- src/test/java/NoticeBoard/FuncTest.java
+++ src/test/java/NoticeBoard/FuncTest.java
@@ -2,16 +2,28 @@ package NoticeBoard;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import junit.framework.TestCase;
+import java.net.URL;
+
 /**
  * Created by Boots on 28/05/2015.
  */
-public class FuncTest {
+public class FuncTest extends TestCase{
+    private URL siteBase;
+    private WebDriver drv;
 
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        siteBase = new URL("http://localhost:10001/");
+        drv = new FirefoxDriver();
+    }
     @Test
-    /*
-    *  @goal FunctionalTesting
-    */
     public void funcTest() throws Exception{
-        assertEquals(1,1);
+        assertTrue(drv.getPageSource().contains("New Announcement"));
     }
 }
