3779781940f8bcf30ae8d94f42a0d5f247efa7c0
Álvaro <alvaro.castro@u-tad.com>
2015-05-28 09:46:42 +0200

    Webdriver chrome still not working

--- src/test/java/NoticeBoard/FuncTestIT.java
+++ src/test/java/NoticeBoard/FuncTestIT.java
@@ -2,24 +2,20 @@ package NoticeBoard;
 
 import static org.junit.Assert.*;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.junit.Ignore;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import java.net.URL;
 
 import org.junit.Test;
 
 public class FuncTestIT{
-    @Ignore("Webdriver not working")
+    @Ignore ("Firefox headless not working")
     @Test
     public void mainPageTest() throws Exception{
 
-        ChromeDriverManager.setup();
-        WebDriver drv = new ChromeDriver();
         URL siteBase = new URL("http://localhost:10001/");
-
+        WebDriver drv = new FirefoxDriver();
 
         drv.get(siteBase.toString());
         assertTrue(drv.getPageSource().contains("Notice Board"));
