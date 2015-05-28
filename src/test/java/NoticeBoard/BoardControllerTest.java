eb39bc912147f546b3f8a4be463f6e338f6210ff
Guillermo Platas <elbotas@gmail.com>
2015-05-28 03:26:11 +0200

    Starting with tests

---
+++ src/test/java/NoticeBoard/BoardControllerTest.java
@@ -0,0 +1,31 @@
+package NoticeBoard;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Boots on 28/05/2015.
+ */
+public class BoardControllerTest {
+
+    @Test
+    public void testMain() throws Exception {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testNewAnnouncement() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void testView() throws Exception {
+        assertEquals(1,1);
+    }
+}
